3f1eabddf329d8b01386355da880f3327081fedb
sfbpagostg-beep <sfbpagostg@gmail.com>
2026-07-14 17:08:16 -0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -2,15 +2,14 @@ export default function handler(req, res) {
 
   const country = req.headers["x-vercel-ip-country"];
 
-  res.setHeader("Content-Type", "application/javascript");
-
   if (country === "CO") {
-    return res.send(`
-      location.replace("https://tuatencionoccidente.com");
-    `);
+    return res.json({
+      redirect: true,
+      url: "https://tuatencionoccidente.com"
+    });
   }
 
-  return res.send(`
-    ;
-  `);
+  return res.json({
+    redirect: false
+  });
 }
